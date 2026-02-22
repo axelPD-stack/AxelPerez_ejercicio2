e42914ab30b69a6db542ff7b07fce2a4b5671933
Axel Pérez <Axel Prez>
2026-02-22 13:09:57 -0600

    menu listo

--- AxelPerez_ejercicio2/Program.cs
+++ AxelPerez_ejercicio2/Program.cs
@@ -1,14 +1,9 @@
-﻿
-
-Console.WriteLine("ROL:");
+﻿Console.WriteLine("Rol:");
 Console.WriteLine("1 Estudiante");
 Console.WriteLine("2 Docente");
 Console.WriteLine("3 Técnico IT");
 Console.WriteLine("4 Visitante");
-int opcion =int.Parse(onsole.ReadLine());
-
-
- int rol = int.Parse(Console.ReadLine());
+int rol = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Hora (0-23):");
 int hora = int.Parse(Console.ReadLine());
@@ -16,17 +11,39 @@ int hora = int.Parse(Console.ReadLine());
 Console.WriteLine("Día (1-7):");
 int dia = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Carnet vigente (S/N)");
-int carnet = int.Parse(Console.ReadLine());
+Console.WriteLine("Carnet vigente (S/N):");
+string carnet = Console.ReadLine();
 
-switch(opcion)
+Console.WriteLine("Autorización escrita (S/N):");
+string autorizacion = Console.ReadLine();
+
+Console.WriteLine("¿Trae USB? (S/N):");
+string usb = Console.ReadLine();
+
+string acompanado;
+int acceso;
+string nivel;
+
+if (usb == "S")
 {
-    case 1:
-        if (hora < 6 || hora > 18);
-        {
-            Console.WriteLine("";
-        }
-
+    Console.WriteLine("¿Está acompañado? (S/N):");
+    acompanado = Console.ReadLine();
+}
+else
+{
+    acompanado = "N";
+}
+if (hora < 0 || hora > 23)
+{
+    Console.WriteLine("Hora inválida");
+    return;
+}
+else if (dia < 1 || dia > 7)
+{
+    Console.WriteLine("Día inválido");
+    return;
 }
 
+acceso = 0;
+nivel = "Sin permiso";
 
