adbfefe9dff478ff53a0ff3068380ef4d457ab96
Axel Pérez <Axel Prez>
2026-02-22 12:29:19 -0600

    creacion del proyecto

--- AxelPerez_ejercicio2/Program.cs
+++ AxelPerez_ejercicio2/Program.cs
@@ -1,1 +1,32 @@
 ﻿
+
+Console.WriteLine("ROL:");
+Console.WriteLine("1 Estudiante");
+Console.WriteLine("2 Docente");
+Console.WriteLine("3 Técnico IT");
+Console.WriteLine("4 Visitante");
+int opcion =int.Parse(onsole.ReadLine());
+
+
+ int rol = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Hora (0-23):");
+int hora = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Día (1-7):");
+int dia = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Carnet vigente (S/N)");
+int carnet = int.Parse(Console.ReadLine());
+
+switch(opcion)
+{
+    case 1:
+        if (hora < 6 || hora > 18);
+        {
+            Console.WriteLine("";
+        }
+
+}
+
+
